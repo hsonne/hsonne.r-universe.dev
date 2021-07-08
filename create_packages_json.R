d75bbc8d409868159f8b52da0549220fefac2e0d
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2021-07-08 12:30:00 +0200

    Remove repos that are not R packages!

--- create_packages_json.R
+++ create_packages_json.R
@@ -2,9 +2,24 @@ all_meta <- jsonlite::read_json("https://api.github.com/users/hsonne/repos")
 
 keys <- sapply(all_meta, kwb.utils::selectElements, "name") 
 
-meta <- all_meta[! keys %in% c("universe", "mrustl.github.io")]
+meta <- all_meta[! keys %in% c(
+  "codemetar",
+  "fine", 
+  "flusshygiene-opencpu-base",
+  "ggplot2",
+  "haus-vom-nikolaus",
+  "mietspiegel",
+  "mrustl.github.io",
+  "pFromGrADS",
+  "pptmerge",
+  "r2r",
+  "rain",
+  "Testprojekt_01",
+  "universe",
+  "xtruso_R"
+)]
 
-name_url <- lapply(all_meta, function(x) {
+name_url <- lapply(meta, function(x) {
   get <- kwb.utils::createAccessor(x)
   list(package = get("name"), url = get("url"))
 })
